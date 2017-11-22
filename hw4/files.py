70e76c53ca6e244398faba1b609ee90a9f846368
maxxkim <31654141+maxxkim@users.noreply.github.com>
2017-11-22 18:30:44 +0300

    Домашнее задание по файлам

---
+++ hw4/files.py
@@ -0,0 +1,17 @@
+f = open("/Users/Max/Desktop/text.txt", encoding="utf-8")
+text = f.read()
+f.close()
+text.split(" ")
+c1 = 0
+c2 = 0
+for word in text:
+    n = len(word)-1
+    if word[n] == ".":
+        c1 += 1
+    elif word[n] == ",":
+        c1 += 1
+    else:
+        c2 += 1
+    print(word)
+res = c1/(c2+c1)*100
+print("Процент слов, заканчивающихся точкой или запятой =", res, "%")

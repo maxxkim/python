b53090a7b8d6824bf607ace6640a252b5248c387
maxxkim <31654141+maxxkim@users.noreply.github.com>
2018-02-04 21:39:01 +0300

    Update dzha.py

--- hw7/dzha.py
+++ hw7/dzha.py
@@ -1,5 +1,5 @@
 def text(name):
-    with open (name + ".txt", "r", encoding = "utf-8") as f:
+    with open ("/Users/Max/Desktop/" + name + ".txt", "r", encoding = "utf-8") as f:
         return (f.read()).split(" ")
 
 def count(text):
@@ -9,6 +9,11 @@ def count(text):
             count[0] += 1
             if word[-3:] == "ied":
                 count[1] += 1
+        elif word[-1:] == "." or word[-1:] == "," or word[-1:] == "!" or word[-1:] == "?":
+            if word[-3:-1] == "ed":
+                count[0] += 1
+                if word[-4:-1] == "ied":
+                    count[1] += 1
     return count
 
 filename = input("Введите имя файла с текстом (без расширения): ")
@@ -17,4 +22,4 @@ count = count(text)
 print("Словоформ на '-ed': ",count[0])
 print("Словоформ на '-ied': ",count[1])
 
-#а причем тут словари? 
+#а причем тут словари?
